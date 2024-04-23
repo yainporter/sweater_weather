c8e7c710a81f2df9d89c69eaab7fbc8f4382224e
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-23 10:25:31 -0700

    fix: Move YelpService to OutsideApiService

--- app/services/outside_api_service.rb
+++ app/services/outside_api_service.rb
@@ -8,11 +8,6 @@ class OutsideApiService
     end
   end
 
-  def get_lat_lng(location)
-      response = mapquest_conn.get("/geocoding/v1/address?location=#{location}")
-      response.body
-  end
-
   def weather_conn
     Faraday.new(url: "http://api.weatherapi.com/v1/") do |faraday|
       faraday.params["key"] = Rails.application.credentials[:weather_api]
@@ -20,6 +15,26 @@ class OutsideApiService
       faraday.response :json, parser_options: { symbolize_names: true }
     end
   end
+
+  def yelp_conn
+    Faraday.new(url: "https://api.yelp.com/") do |faraday|
+      faraday.headers["Authorization"] = "Bearer KS7IFxwWZlVQRR_ryycSwBLyayF-js-3Haeqj6X4tswJ9I_9FbRuhob-1nQNeP6E96gtz1AMCPjOcxtbuoLiMgEUFe_8ouUdBNu4DtgsmzyGmGEfKQoDxnHPHOknZnYx"
+      faraday.headers["Accept"] = "application/json"
+      faraday.request :json
+      faraday.response :json, parser_options: { symbolize_names: true }
+    end
+  end
+
+  def get_yelp_restaurants(location)
+    response = yelp_conn.get("/v3/businesses/search?location=#{location}&sort_by=best_match&limit=20")
+    response.body
+  end
+
+  def get_lat_lng(location)
+      response = mapquest_conn.get("/geocoding/v1/address?location=#{location}")
+      response.body
+  end
+
 
   def get_weather(lat_lng)
     response = weather_conn.get("forecast.json?q=#{lat_lng}&days=5")
--- spec/services/outside_api_service_spec.rb
+++ spec/services/outside_api_service_spec.rb
@@ -74,4 +74,43 @@ RSpec.describe OutsideApiService do
       expect(data[:route].keys).to eq(route_keys)
     end
   end
+
+  describe "#yelp_conn" do
+    it "connects to the yelp api with Faraday", :vcr do
+      connection = service.yelp_conn
+      expect(connection).to be_a(Faraday::Connection)
+    end
+  end
+
+  describe "#get_yelp_restaurants" do
+    it "returns data for a restaurant based on location", :vcr do
+      data = service.get_yelp_restaurants("Phoenix")
+
+      data_keys = [:businesses, :total, :region]
+      business_keys = [
+                        :id,
+                        :alias,
+                        :name,
+                        :image_url,
+                        :is_closed,
+                        :url,
+                        :review_count,
+                        :categories,
+                        :rating,
+                        :coordinates,
+                        :transactions,
+                        :price,
+                        :location,
+                        :phone,
+                        :display_phone,
+                        :distance,
+                        :attributes]
+
+      expect(data).to be_a(Hash)
+      expect(data.keys).to eq(data_keys)
+      expect(data[:businesses]).to be_an(Array)
+      expect(data[:businesses].count).to eq(20)
+      expect(data[:businesses].first.keys).to eq(business_keys)
+    end
+  end
 end
